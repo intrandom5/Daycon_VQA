046404c62391b84a63f4ec122f57964382a6cbc4
intrandom5 <random_5@naver.com>
2023-07-20 18:05:37 +0900

    dataset.py: fix len(self.img_feats)->len(self.df)

--- dataset.py
+++ dataset.py
@@ -9,7 +9,7 @@ class VQADataset(Dataset):
         self.is_test = is_test
 
     def __len__(self):
-        return len(self.img_feats)
+        return len(self.df)
 
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
@@ -59,7 +59,7 @@ class VLT5_Dataset(Dataset):
         self.is_test = is_test
 
     def __len__(self):
-        return len(self.img_feats)
+        return len(self.df)
     
     def __getitem__(self, idx):
         row = self.df.iloc[idx]
