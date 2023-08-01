1fe588821ffdffb7cba4d62242a3967df7e46d72
intrandom5 <random_5@naver.com>
2023-08-01 15:11:16 +0900

    main.py: move training process to vlt5_utils.py

--- main.py
+++ main.py
@@ -1,110 +1,40 @@
-import os
-import glob
-import wandb
-import argparse
-import pandas as pd
-
 import torch
-import torch.nn as nn
-import torch.optim as optim
-
-from model import get_vlt5
-from transformers import T5Tokenizer
-from utils import load_pickles, prepare_data, train_vlt5, inference_vlt5
+import argparse
+from vlt5_utils import vlt5_process
 
 
 def main(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    args.device = device
 
     assert args.model_type in ["vlt5"]
 
-    tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-    vocab_size = len(tokenizer)
-
-    # open pickle files
-    print("load FRCNN features...")
-    img_feat_pkls = sorted(glob.glob(args.train_img_path+"/*.pkl"))
-    train_img_feats = load_pickles(img_feat_pkls)
-
-    bboxes = sorted(glob.glob(args.train_bbox_path+"/*.pkl"))
-    train_bboxes = load_pickles(bboxes)
-    print("Done!")
-
-    # define Dataloader
     if args.model_type == "vlt5":
-        train_loader = prepare_data(
-            args.train_df,
-            tokenizer,
-            test_mode=False,
-            shuffle=True,
-            img_feats=train_img_feats,
-            bboxes=train_bboxes
-        )
-        valid_loader = prepare_data(
-            args.valid_df,
-            tokenizer,
-            test_mode=False,
-            shuffle=False,
-            img_feats=train_img_feats,
-            bboxes=train_bboxes
-        )
+        vlt5_process(args)
+
+    # print("load test FRCNN features...")
+    # img_feat_pkls = sorted(glob.glob(args.test_img_path+"/*.pkl"))
+    # test_img_feats = load_pickles(img_feat_pkls)
+    # if args.model_type == "vlt5":
+    #     bboxes = sorted(glob.glob(args.test_bbox_path+"/*.pkl"))
+    #     test_bboxes = load_pickles(bboxes)
+    #     test_loader = prepare_data(
+    #         args.test_df, 
+    #         tokenizer=tokenizer,
+    #         test_mode=True,
+    #         shuffle=False,
+    #         img_feats=test_img_feats,
+    #         bboxes=test_bboxes
+    #     )
+    #     print("Done!")
+    #     preds = inference_vlt5(model, test_loader, device)
 
 
-    # Define Model
-    if args.model_type == "vlt5":
-        model = get_vlt5("google/flan-t5-base")
-        
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=args.learning_rate)
+    # no_pad_output = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
-    if not os.path.exists(args.model_path):
-        os.mkdir(args.model_path)
-
-    model.to(device)
-    if args.logger == "wandb":
-        run = wandb.init(
-            project="DayCon_VQA", 
-            entity="intrandom5", 
-            name=args.log_name, 
-            notes=args.log_note
-        )
-
-    # Training Start!
-    if args.model_type == "vlt5":
-        train_vlt5(
-            model, 
-            train_loader, 
-            valid_loader, 
-            optimizer, 
-            tokenizer.pad_token_id, 
-            device,
-            args.epochs,
-            args.model_path
-        )
-
-    print("load test FRCNN features...")
-    img_feat_pkls = sorted(glob.glob(args.test_img_path+"/*.pkl"))
-    test_img_feats = load_pickles(img_feat_pkls)
-    if args.model_type == "vlt5":
-        bboxes = sorted(glob.glob(args.test_bbox_path+"/*.pkl"))
-        test_bboxes = load_pickles(bboxes)
-        test_loader = prepare_data(
-            args.test_df, 
-            tokenizer=tokenizer,
-            test_mode=True,
-            shuffle=False,
-            img_feats=test_img_feats,
-            bboxes=test_bboxes
-        )
-        print("Done!")
-        preds = inference_vlt5(model, test_loader, device)
-
-
-    no_pad_output = tokenizer.batch_decode(preds, skip_special_tokens=True)
-
-    sample_submission = pd.read_csv('../sample_submission.csv')
-    sample_submission["answer"] = no_pad_output
-    sample_submission.to_csv(args.submission_name, index=False)
+    # sample_submission = pd.read_csv('../sample_submission.csv')
+    # sample_submission["answer"] = no_pad_output
+    # sample_submission.to_csv(args.submission_name, index=False)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
