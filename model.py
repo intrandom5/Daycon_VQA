7c5630972459f3b09a12bd7d4f99b0601b8fca74
intrandom5 <random_5@naver.com>
2023-08-01 15:14:08 +0900

    model.py: add git model

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from transformers import T5Config
+from transformers import T5Config, AutoModelForCausalLM
 from modeling_t5 import VLT5
 
     
@@ -12,3 +12,6 @@ def get_vlt5(model_name):
     t5_config.use_vis_order_embedding = True
     t5_config.tie_word_embeddings=True
     return VLT5.from_pretrained(model_name, config=t5_config)
+
+def get_git():
+    return AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
